aa6015fcfb22efa0afc0603fe8769cbdf5b9d46b
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-23 06:10:18 +0300

    Adding updating methods for url

--- express_server.js
+++ express_server.js
@@ -31,19 +31,18 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   let longURL = req.body.longURL;
-  if (!longURL.includes('http')) {
-    longURL = `http://${longURL}`
-  }
   urlDatabase[shortURL] = longURL;
-  const templateVars = { shortURL, longURL };
-  res.render("urls_show", templateVars);
+  res.redirect(`urls/${shortURL}`);
 });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL
   const templateVars = { shortURL, longURL: urlDatabase[req.params.shortURL] };
@@ -53,15 +52,23 @@ app.get("/urls/:shortURL", (req, res) => {
     res.status(400).send("Caution!!! Please check your URL list for the short URL.")
   }
 });
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+app.post('/urls/:shortURL', (req, res) => {
+  let shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.editedLongURL;
+  res.redirect("/urls");
+});
+
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect("/urls");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
